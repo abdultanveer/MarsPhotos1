9ee43038b1f5b198dd2ea5a4b8cbe4df32da9187
abdul <abdul.tanveer@gmail.com>
2025-03-19 10:04:56 +0530

    taking runtime permission

--- app/src/main/java/com/example/marsphotos/ServiceActivity.kt
+++ app/src/main/java/com/example/marsphotos/ServiceActivity.kt
@@ -1,17 +1,23 @@
 package com.example.marsphotos
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ListView
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class ServiceActivity : AppCompatActivity() {
     lateinit var  inboxListView:ListView
+    private val SMS_PERMISSION_CODE = 101 //request code
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
@@ -35,5 +41,26 @@ class ServiceActivity : AppCompatActivity() {
     fun stoptMusicService(view: View) {
         var mIntent = Intent(this,MusicService::class.java)
         stopService(mIntent)
+    }
+
+    private fun checkSmsPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestSmsPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(
+            android.Manifest.permission.READ_SMS),
+            SMS_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == SMS_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //readSmsInbox()
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
